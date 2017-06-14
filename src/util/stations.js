5a8ad408d85970837e4285a08376e65d65969e68
Jonathan MacDonald <jon.r.macdon@gmail.com>
2017-06-13 19:11:34 -0500

    added bearing and distance to nearest stations

--- src/util/stations.js
+++ src/util/stations.js
@@ -3,10 +3,11 @@ export function getNearestStations(lat, long) {
   var stationDist = stations.map((station) => {
     return {
       "icao": station.icao,
-      "dist": distanceFactorBetweenCoordinates(station.lat, station.long, lat, long)
+      "statuteMiles": distanceBetweenCoordinates(station.lat, station.long, lat, long),
+      "bearing": initialBearingBetweenCoordinates(lat, long, station.lat, station.long)
     }
   }).sort((a, b) => {
-    return a.dist - b.dist
+    return a.statuteMiles - b.statuteMiles
   })
   return stationDist
 }
@@ -15,7 +16,23 @@ function degreesToRadians(degrees) {
   return degrees * Math.PI / 180
 }
 
-function distanceFactorBetweenCoordinates(lat1, long1, lat2, long2) {
+function radiansToDegrees(radians) {
+  return (180 * radians) / Math.PI
+}
+
+function initialBearingBetweenCoordinates(lat1, long1, lat2, long2) {
+  var dLong = degreesToRadians(long2 - long1)
+  lat1 = degreesToRadians(lat1)
+  lat2 = degreesToRadians(lat2)
+
+  var y = Math.sin(dLong) * Math.cos(lat2)
+  var x = Math.cos(lat1) * Math.sin(lat2) -
+          Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLong)
+  return radiansToDegrees(Math.atan2(y, x))
+}
+
+function distanceBetweenCoordinates(lat1, long1, lat2, long2) {
+  const earthRadius = 3959 //sm
   var dLat = degreesToRadians(lat2-lat1)
   var dLong = degreesToRadians(long2-long1)
 
@@ -25,7 +42,7 @@ function distanceFactorBetweenCoordinates(lat1, long1, lat2, long2) {
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
           Math.sin(dLong / 2) * Math.sin(dLong /2) * Math.cos(lat1) * Math.cos(lat2)
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-  return c
+  return c * earthRadius
 }
 
 const stations = [{"icao": "PADK", "lat": 51.88333333, "long": -176.65},
